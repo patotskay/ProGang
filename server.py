a2837866e159f1941c49458e0b84a762c0e9fd20
termik88 <dirkser@mail.ru>
2023-04-08 12:52:22 +0500

    Перенос строки исправлен flake8

---
+++ server.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+from transformers import pipeline
+from pydantic import BaseModel
+
+from logger import MyLOgger11
+
+
+class Item(BaseModel):
+    text: str
+
+
+app = FastAPI()
+classifier = pipeline("sentiment-analysis")
+logger = MyLOgger11("./log.txt")
+
+
+@app.get("/")
+def root():
+    logger.write("get root request", console=True)
+    return {"message": "Hello World"}
+
+
+@app.post("/predict/")
+def predict(item: Item):
+    logger.write(
+        f"get root predict, result: {classifier(item.text)[0]}",
+        console=True
+        )
+    return classifier(item.text)[0]
